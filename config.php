95eaec91a3294e93b1eb19296fbffa2ea7887d5f
vardhanreddy27 <63119222+vardhanreddy27@users.noreply.github.com>
2021-05-21 01:42:47 +0530

    Update config.php

--- config.php
+++ config.php
@@ -9,10 +9,10 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
-$google_client->setClientId('745469838070-ttve30a2vnokb2tpibda761gql4lhncl.apps.googleusercontent.com');
+$google_client->setClientId('702455199710-hhlq11gb4mh6sbrplg7cl57mk0td35kd.apps.googleusercontent.com');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('2iuJkRrp_YokpuQ6Iumg86q5');
+$google_client->setClientSecret('vzEf4E5R_vRAug81u_9lCxf3');
 
 //Set the OAuth 2.0 Redirect URI
 $google_client->setRedirectUri('https://radiant-beyond-33119.herokuapp.com/gindex.php');
