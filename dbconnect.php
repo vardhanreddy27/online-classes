600d893c04d83c1a550a1b8520f8d89096a435a0
vardhanreddy27 <63119222+vardhanreddy27@users.noreply.github.com>
2021-05-21 02:00:53 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -4,7 +4,7 @@
     $servername="sql6.freemysqlhosting.net";
     $username="sql6413737";
     $password="uN9H3KbsDa";
-    $dbname="canna";
+    $dbname="sql6413737";
     $conn=new mysqli($servername,$username,$password,$dbname);
     if($conn->connect_error)
     { die("connection failed".$conn->connect_error); }
